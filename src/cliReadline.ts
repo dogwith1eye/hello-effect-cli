44dd88429b5344ec22e63f0de173bafc0ce27aec
dogwith1eye <dogwith1eye@gmail.com>
2023-01-03 18:59:25 -0500

    Added scoped readline

--- src/cliReadline.ts
+++ src/cliReadline.ts
@@ -4,13 +4,17 @@ const { program } = require('commander')
 
 import * as Z from "@effect/io/Effect"
 
-import { readlineProgram } from './programReadline'
+import { readlineProgram, readlineProgramScope } from './programReadline'
 
 program
   .version('0.1.0')
   .parse(process.argv)
 
 const options = program.opts()
-Z.unsafeRunPromise(readlineProgram())
+// Z.unsafeRunPromise(readlineProgram())
+//   .then(result => console.log(result))
+//   .catch(err => console.log(err))
+
+Z.unsafeRunPromise(readlineProgramScope())
   .then(result => console.log(result))
   .catch(err => console.log(err))
--- src/programReadline.ts
+++ src/programReadline.ts
@@ -1,31 +1,33 @@
 import { pipe } from "@fp-ts/data/Function"
+import * as Context from "@fp-ts/data/Context";
 import * as Z from "@effect/io/Effect"
+import * as Scope from "@effect/io/Scope";
 
 import * as readline from 'node:readline/promises'
 import { stdin as input, stdout as output } from 'node:process'
+import { promisify } from "node:util";
 
 const createInterface = () =>
-  Z.succeed(readline.createInterface({ input, output }))
+  Z.sync(() => readline.createInterface({ input, output }))
 
-const askQuestion = (rl:readline.Interface) =>
+const askQuestion = (rl: readline.Interface) =>
   Z.tryCatchPromise(
     () => rl.question("What do you think of Node.js? "), // Promise<string>
     () => "askQuestion err" as const
   )
 
-const printAnswer = (answer:string) =>
-  Z.succeed(console.log(`Thank you for your valuable feedback: ${answer}`))
+const printAnswer = (answer: string) =>
+  Z.sync(() => console.log(`Thank you for your valuable feedback: ${answer}`))
 
-const close = (rl:readline.Interface) =>
-  Z.succeed(rl.close())
+const askAndPrint = (rl: readline.Interface) =>
+  pipe(
+    askQuestion(rl),
+    // Z.flatMap((answer) => printAnswer(answer))
+    Z.flatMap(printAnswer)
+  )
 
-// export const readlineProgram = () =>
-//   pipe(
-//     createInterface(),
-//     Z.flatMap(askQuestion),
-//     Z.flatMap(printAnswer)
-//     Z.flatMap(close) //don't have access to rl
-//   );
+const close = (rl: readline.Interface) =>
+  Z.sync(() => rl.close())
 
 export const readlineProgram = () =>
   Z.gen(function* ($) {
@@ -34,3 +36,30 @@ export const readlineProgram = () =>
     const _ = yield* $(printAnswer(answer))
     yield* $(close(rl))
   })
+
+export interface ReadlineInterface {
+  readonly rl: readline.Interface;
+}
+
+export const FileDescriptor = Context.Tag<ReadlineInterface>();
+
+export const resource: Z.Effect<Scope.Scope, never, ReadlineInterface> =
+  Z.acquireRelease(
+    pipe(
+      Z.sync(() => readline.createInterface({ input, output })),
+      Z.map((rl) => ({ rl })),
+      Z.tap(() => Z.logInfo("ReadlineInterface acquired"))
+    ),
+    ({ rl }) =>
+      pipe(
+        Z.sync(() => rl.close()),
+        Z.tap(() => Z.logInfo("ReadlineInterface released"))
+      )
+  )
+
+export const readlineProgramScope = () =>
+  pipe(
+    resource,
+    Z.flatMap((_) => askAndPrint(_.rl)),
+    Z.scoped
+  )
